063dc6d39df0b7792045371a17020bb414ede3af
Daniel Juarez <juarezz.daniel97@gmail.com>
2025-05-11 17:30:50 -0300

    Login con estilos terminado

---
+++ src/components/forms/FormLogin.jsx
@@ -0,0 +1,81 @@
+import { useForm } from "react-hook-form"
+import { useAuthContext } from "../../contexts/AuthContext";
+import { useNavigate } from "react-router-dom";
+import { Loader2 } from "lucide-react";
+
+
+const FormLogin = () => {
+    const {  login, loading, error } = useAuthContext(); 
+    const { register, handleSubmit, formState:{errors} } = useForm();
+    
+    const navigate = useNavigate();
+
+    const onSubmit = async (data) => {
+        const {email, password} = data;
+        const response = await login(email, password);
+
+        if (response) {
+            navigate("/patient");
+        }
+    }
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)}>
+            {
+                error && (
+                    <div className="mb-4 p-3 bg-red-100 dark:bg-red-900/30 text-red-700 dark:text-red-400 rounded-md text-sm">
+                        {error}
+                    </div>)
+            }
+            
+            <div className="mb-4">
+                <label htmlFor="email" className="block text-gray-700 dark:text-gray-300 text-sm font-medium mb-2">
+                    Correo electrónico
+                </label>
+                <input 
+                    id="email"
+                    type="text"
+                    className="w-full px-3 py-2 border rounded-md bg-white text-gray-900  dark:bg-gray-700 dark:text-white" 
+                    placeholder="tu@ejemplo.com"
+                    {...register("email", {required: "El correo electrónico es obligatorio"})}
+                />
+                {errors.email && (
+                    <p className="mt-1 text-red-500 text-xs">{errors.email.message}</p>
+                )}
+            </div>
+
+            <div className="mb-6">
+                <label htmlFor="password" className="block text-gray-700 dark:text-gray-300 text-sm font-medium mb-2">
+                    Contraseña
+                </label>
+                <input
+                    id="password" 
+                    type="password" 
+                    className="w-full px-3 py-2 border rounded-md bg-white text-gray-900  dark:bg-gray-700 dark:text-white"
+                    placeholder="*********"
+                    {...register("password", {required: "La contraseña es obligatoria", minLength:{ value:3, message:"La contraseña debe tener al menos 3 caracteres"}})}
+                />
+                {errors.password && (
+                    <p className="mt-1 text-red-500 text-xs">{errors.password.message}</p>
+                )}
+            </div>
+
+            <button
+                type="submit"
+                disabled={loading}
+                className="w-full bg-emerald-600 text-white py-2 px-4 rounded-md hover:bg-emerald-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-emerald-500 transition duration-150 ease-in-out"
+            >
+                {loading ? (
+                    <span className="flex items-center justify-center">
+                        <Loader2 className="animate-spin mr-2" />
+                        Iniciando sesión...
+                    </span>
+                    ) : (
+                        'Iniciar sesión'
+                    )}
+            </button>
+        </form>
+    )
+}
+
+export default FormLogin
--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,76 +1,34 @@
-import { useAuthContext } from '../../contexts/AuthContext';
-import { useForm } from 'react-hook-form';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Header from '../../components/layout/Header';
 import Footer from '../../components/layout/Footer';
+import FormLogin from '../../components/forms/FormLogin';
 
 const Login = () => {
-    const { login, loading, error } = useAuthContext();
-    const {register, handleSubmit, formState:{errors}} = useForm();
-
-    const navigate = useNavigate();    
-
-    const onSubmit = async (data) => {
-        const { email, password } = data;
-        const response = await login(email, password);
-        
-        if(response){
-            navigate("/patient");
-        }
-    }
 
     return (
         <>
         <Header/>
         
-        <div className='flex flex-col justify-center items-center h-screen bg-gray-200 '>
-            <div className='bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 w-96'>
-                <h1 className='font-medium mb-4 text-center'>Login</h1>
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <div>
-                        {error && <span className='text-red-500 text-sm text-center mb-4'>{error}</span>}
-                        {/* {success && <span className='text-green-500 text-sm'>{success}</span>} */}
-                    </div>
-                    <input 
-                        type="text" 
-                        className='border border-gray-300 rounded p-2 mb-4 w-full'
-                        placeholder="Email"
-                        {...register("email", {required: true})}
-                        />
-                    {errors.email && <span className='text-red-500 text-sm'>Email is required</span>}
+        <main className='flex flex-col justify-center items-center h-screen bg-gray-200 dark:bg-gray-800 '>
+            <div  className="bg-white dark:bg-gray-900 p-4 sm:p-6 md:p-8 rounded-lg shadow-md w-full max-w-md">
+                <div className="text-center mb-6 sm:mb-8">
+                    <h1 className="text-xl sm:text-2xl font-bold text-emerald-600 dark:text-emerald-400">MediCat</h1>
+                    <p className="text-gray-600 dark:text-gray-300 mt-2 text-sm sm:text-base">Inicia sesión en tu cuenta</p>
+                </div>
 
-                    <input 
-                        type="password" 
-                        className='border border-gray-300 rounded p-2 mb-4 w-full'
-                        placeholder="Password"
-                        {...register("password", {required: true})}
-                        />
+                {/* Formulario */}
+                    <FormLogin/>
 
-                    {errors.password && <span className='text-red-500 text-sm'>Password is required</span>}
-
-                    <button
-                        type='submit'
-                        className='bg-blue-500 text-white rounded p-2 w-full cursor-pointer hover:bg-blue-600 transition duration-200'
-                    >
-                        Iniciar Sesion
-                    </button>
-                </form>
-                <p className='text-sm text-center mt-4'>
-                    admin@gmail.com <br />
-                    admin
-                    <hr />
-                    medico@gmail.com <br />
-                    medico
-                    <hr />
-                    paciente@gmail.com <br />
-                    paciente
-                </p>
-                <p className='text-sm text-center mt-4'>
-                    ¿No tienes una cuenta? <br />
-                    <span className='text-blue-500 cursor-pointer' onClick={() => navigate("/register")}>Registrate</span>
-                </p>
+                <div className="mt-6 text-center">
+                    <p className="text-sm text-gray-600 dark:text-gray-300">
+                        ¿No tienes una cuenta?{' '}
+                        <Link to="/register" className='text-emerald-600 cursor-pointer dark:text-emerald-400 hover:text-emerald-500 dark:hover:text-emerald-300 font-medium'>
+                            Regístrate aquí
+                        </Link>
+                    </p>
+                </div>
             </div>
-        </div>
+        </main>
 
         <Footer/>
         </>
