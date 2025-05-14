a1a65ae258c33baa7d3ca47c4be87fa0ce2aa139
Daniel Juarez <juarezz.daniel97@gmail.com>
2025-05-14 16:58:09 -0300

    Estilos en Agenda Medico

--- src/pages/medico/AgendaMedico.jsx
+++ src/pages/medico/AgendaMedico.jsx
@@ -1,3 +1,4 @@
+import { Calculator, Calendar, Clock, Edit, Loader2 } from "lucide-react";
 import { useMedicoContext } from "../../contexts/MedicoContext"
 
 
@@ -5,33 +6,104 @@ const AgendaMedico = () => {
     const {loading, error, agenda} = useMedicoContext();
 
     return (
-        <div>
-            <h1>AgendaMedico</h1>
-            <p>Esta es la página de agenda de turnos asociado al médico.</p>
+        <>
+        <div className="bg-white dark:bg-gray-700 rounded-lg max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6 shadow-md p-6">
+            <div>
+                filtros
+            </div>
+            <div className="space-y-4">
+                {
+                    loading ? (
+                        <span className='flex items-center justify-center'> 
+                            <Loader2 className='animate-spin mr-2'/> Cargando...
+                        </span>
+                    ) : (
+                        <div>
+                            {/* Botones - filtros */}
+                            <div className="flex flex-col sm:flex-row justify-between items-center mb-6">
+                                <div className="flex items-center mb-4 sm:mb-0">
+                                    <button>izq</button>
+                                    <h2>fecha</h2>
+                                    <button>der</button>
+                                </div>
 
-            <div className='flex justify-between items-center bg-gray-200 p-4'>
-                {loading ? (
-                    <p>Cargondo...</p>
-                ) : (
-                    <ul>
-                        {
-                        agenda && agenda.length > 0 ? (
-                            agenda.map((turno, index) => (
-                                <li key={index} className='p-2 m-2 bg-gray-100 rounded'>
-                                <p>Fecha: {turno.fecha}</p>
-                                <p>Paciente: {turno.paciente.nombreCompleto}</p>
-                                <p>estado: {turno.estado}</p>
-                                <p>Motivo: {turno.motivo}</p>
-                            </li>
-                            ))) : (
-                                <li>No tienes turnos programados.</li>
-                            )
-                        }
-                    </ul>
-                )
+                                <div className="flex items-center space-x-2">
+                                    <button>Hoy</button>
+                                    <button>Ver calendario</button>
+
+                                </div>
+                            </div>
+
+                            {/* Resumen agendas - header*/}
+                            <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-6">
+                                <div className="p-4 rounded-lg bg-gray-100 dark:bg-gray-600">
+                                    <div className="flex justify-between items-center">
+                                        <h3 className="text-sm font-medium text-gray-500 dark:text-gray-200">Total de Citas</h3>
+                                        <Calendar className="h-5 w-5 text-emerald-600 dark:text-emerald-400"/>
+                                    </div>
+                                    <p className="text-2xl font-bold mt-2 text-gray-800 dark:text-white">{ agenda?.length || 0 }</p>
+                                </div>
+                                <div className="p-4 rounded-lg bg-gray-100 dark:bg-gray-600">
+                                    confirmadas
+                                </div>
+                                <div className="p-4 rounded-lg bg-gray-100 dark:bg-gray-600">
+                                    Pendientes
+                                </div>
+                            </div>
+
+                            {/*Listas de Agendas */}
+                            <div className="space-y-4">
+                                {
+                                    agenda && agenda.length > 0 ? (
+                                        agenda.map((cita, index) => (
+                                            <div 
+                                                key={index} 
+                                                className="p-4 border rounded-lg flex flex-col sm:flex-row sm:items-center justify-between border-gray-200 hover:bg-gray-50 dark:border-gray-600  dark:hover:bg-gray-600/50"
+                                                >
+                                                <div className='flex flex-col sm:flex-row sm:items-center mb-3 sm:mb-0'>
+                                                    <div className='flex items-center mb-2 sm:mb-0 sm:mr-4'>
+                                                        <div className={`h-10 w-10 rounded-full flex items-center justify-center mr-3 ${
+                                                                cita.estado === 'confirmada' ? 'bg-green-100 text-green-600 dark:bg-green-900/30 dark:text-green-400' : 'bg-yellow-100 text-yellow-600 dark:bg-yellow-900/30 dark:text-yellow-400'
+                                                                }`}>
+                                                            <Clock className="h-5 w-5" />
+                                                        </div>
+                                                        <div>
+                                                            <h3 className="font-medium text-gray-900 dark:text-white">{cita.paciente.nombreCompleto}</h3>
+                                                            <p className="text-sm text-gray-500 dark:text-gray-400">{cita.motivo}</p>
+                                                        </div>
+                                                    </div>
+                                                    <div className='flex items-center text-sm text-gray-500 dark:text-gray-400'>
+                                                        <Calendar className="h-4 w-4 mr-1" />
+                                                        {cita.fecha}
+                                                    </div>
+                                                </div>
+                                                <div className='flex items-center space-x-2'>
+                                                    <span className={`px-2 py-1 text-xs rounded-full ${
+                                                            cita.estado === 'confirmada' 
+                                                            ? 'bg-green-100 text-green-800 dark:bg-green-900/30 dark:text-green-400' 
+                                                            : 'bg-yellow-100 text-yellow-800 dark:bg-yellow-900/30 dark:text-yellow-400'
+                                                        }`}>
+                                                            {cita.estado === 'programado' ? 'Programado' : 'Cancelado'}
+                                                    </span>
+                                                    <button className="text-emerald-600 cursor-pointer dark:text-emerald-400 hover:text-emerald-700 dark:hover:text-emerald-300 text-sm font-medium">
+                                                        <Edit/>
+                                                    </button>
+                                                </div>
+                                            </div>
+                                        ))
+                                    ) : (
+                                        <div className="p-8 text-center rounded-lg bg-gray-50 text-gray-500 dark:bg-gray-700/50 dark:text-gray-400">
+                                            <p className="text-lg font-medium">No hay citas programadas para hoy</p>
+                                        </div>
+                                    )
+                                }
+                            </div>
+                        </div>
+                    )
                 }
             </div>
         </div>
+        </>
     )
 }
 
--- src/pages/medico/HomeMedico.jsx
+++ src/pages/medico/HomeMedico.jsx
@@ -3,10 +3,13 @@ import { useAuthContext } from '../../contexts/AuthContext';
 import { Outlet, useNavigate } from 'react-router-dom';
 import { useMedicoContext } from '../../contexts/MedicoContext';
 import { usePatientContext } from '../../contexts/PatientContext';
+import Header from '../../components/layout/Header';
+import { Calendar, FileText, User, UserCircle2Icon } from 'lucide-react';
+import Footer from '../../components/layout/Footer';
 
 const HomeMedico = () => {
     
-    const { logout } = useAuthContext();
+    const { logout, user } = useAuthContext();
     const { getMedico, setDataMedico, getTurnosAsociados, setAgenda} = useMedicoContext();
     const { ListPatient, setPacientes  } = usePatientContext();
 
@@ -42,56 +45,63 @@ const HomeMedico = () => {
     
 
 return (
-    <div>
-        <div className='flex justify-between items-center bg-gray-200 p-4'>
-            <h1 className='text-2xl font-bold'>Bienvenido - Home Medico</h1>
-            <button
-            onClick={() => handleLogout()}
-            type='button' 
-            className='bg-red-500 text-white rounded p-2 m-2 cursor-pointer'>
-                Cerrar Sesión
-            </button>
+    <>
+        {/* Header */}
+        <Header showLogout={true} onLogout={handleLogout} userEmail={user?.email}/>
+        
+        {/* Navigation */}
+        <div className='min-h-screen bg-gray-200 text-gray-900 dark:bg-gray-800 dark:text-white'>
+                <main className='max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6'>
+                    <div className='mb-6 border-b border-gray-200 dark:border-gray-700'>
+                        
+                        <nav className='flex space-x-8 rounded-lg py-3 px-3 max-w-7xl bg-white dark:bg-gray-700'>
+
+                            {/* Button Agenda */}
+                            <button
+                                className={`py-4 px-1 flex items-center border-b-2 font-medium text-sm cursor-pointer ${ activeTab === 'agenda' ? 'border-emerald-500 text-emerald-600 dark:text-emerald-400' : 'border-transparent text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-300 hover:border-gray-300 dark:hover:border-gray-600' }`}
+                                onClick={() => {
+                                    setActiveTab("agenda");
+                                    navigate("/medico/agenda");
+                                }}
+                            >
+                                <Calendar className="mr-2 h-5 w-5" />
+                                Mi Agenda
+                            </button>
+
+                            {/* Button Medico */}
+                            <button
+                                className={`py-4 px-1 flex items-center border-b-2 font-medium text-sm cursor-pointer ${ activeTab === 'pacientes'  ? 'border-emerald-500 text-emerald-600 dark:text-emerald-400'  : 'border-transparent text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-300 hover:border-gray-300 dark:hover:border-gray-600' }`}
+                                onClick={() => {
+                                    setActiveTab("pacientes");
+                                    navigate("/medico/pacientes");
+                                }}
+                                >
+                                {/* <User className="mr-2 h-5 w-5" /> */}
+                                <FileText className="mr-2 h-5 w-5" />
+                                Mis Pacientes
+                            </button>
+                            
+                            {/* Perfil */}
+                            <button
+                                className={`py-4 px-1 flex items-center border-b-2 font-medium text-sm cursor-pointer ${ activeTab == 'perfil' ? 'border-emerald-500 text-emerald-600 dark:text-emerald-400': 'border-transparent text-gray-500 hover:border-gray-300 dark:text-gray-400 dark:hover:text-gray-300 dark:hover:border-gray-600' }`}
+                                onClick={() => {
+                                    setActiveTab("perfil");
+                                    navigate("/medico/perfil");
+                                }}
+                                >
+                                <UserCircle2Icon className='mr-2 h-5 w-5'/>
+                                Perfil
+                            </button>
+                        </nav>
+                    </div>
+                </main>
+                
+                <Outlet/>
         </div>
 
-        <div className='bg-white shadow-md rounded p-4 m-4'>
-            {/* Navigation */}
-                <div>
-                    <nav>
-                        <button
-                            className={`p-2 m-2 ${activeTab === "agenda" ? "bg-blue-500 text-white" : "bg-gray-200"}`}
-                            onClick={() => {
-                                setActiveTab("agenda");
-                                navigate("/medico/agenda");
-                            }}
-                            >
-                            Agendas
-                        </button>
-
-                        <button
-                            className={`p-2 m-2 ${activeTab === "pacientes" ? "bg-blue-500 text-white" : "bg-gray-200"}`}
-                            onClick={() => {
-                                setActiveTab("pacientes");
-                                navigate("/medico/pacientes");
-                            }}
-                            >
-                            Pacientes
-                        </button>
-
-                        <button
-                            className={`p-2 m-2 ${activeTab === "perfil" ? "bg-blue-500 text-white" : "bg-gray-200"}`}
-                            onClick={() => {
-                                setActiveTab("perfil");
-                                navigate("/medico/perfil");
-                            }}
-                            >
-                            Perfil
-                        </button>
-                    </nav>
-                </div>
-
-                <Outlet/>
-        </div> 
-    </div>
+        {/* Footer */}
+        <Footer/>
+    </>
     )
 }
 
